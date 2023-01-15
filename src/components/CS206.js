155346bd91f36495e9c10ee6799a2bfdcaed5320
tage tapang <tagetage70@gmail.com>
2023-01-15 17:03:00 +0530

    changed favicon

--- src/components/CS206.js
+++ src/components/CS206.js
@@ -4,11 +4,14 @@ import Coresub from './Coresub'
 
 export default function CS206() {
   return (
+    <>
+    
     <div  className="d-flex flex-column">
         <Coresub date= "12 june 2023" firsttitle="mass bunk was conducted due to scarcity of water"/>
         <Coresub date= "13 june 2023" firsttitle="mass bunk was conducted due to scarcity of water"/>
 
       
     </div>
+    </>
   )
 }
